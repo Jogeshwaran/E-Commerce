eaecc63b6a352b34189117aeaf152e807804244e
Jogeshwaran <jokrish923@gmail.com>
2022-07-18 19:20:06 +0530

    Initial Deploy skipped Styled Components

--- src/Components/Card-SubContainer/Card-Subcontainer-Component.jsx
+++ src/Components/Card-SubContainer/Card-Subcontainer-Component.jsx
@@ -1,7 +1,13 @@
+import { useNavigate } from 'react-router-dom';
 import './Card-Subcontainer-styling.scss';
 const CardSubContaiener = ({category}) =>{
+//In order to use the route we can either transform the div to link and pass the route to {to =""}
+// or we could use the useNavigate hook 
+
+  const navigate = useNavigate()
+  const onNavigateHandler = () => navigate(category.route)
     return(
-        <div className="Card-SubContainer">
+        <div className="Card-SubContainer" onClick={onNavigateHandler}>
        <div className='background-image' style={{backgroundImage : `url(${category.imageUrl})`}} /> 
        <div className="Product-SpecificContainer" >
          <h2 >{category.title}</h2>
